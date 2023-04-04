55cddc176c1d7da459af4e8d4a2276d7c6548b9b
unknown <guillermooscarn@gmail.com>
2023-04-04 07:40:44 -0300

    cambiooosss

--- src/models/Vacant.js
+++ src/models/Vacant.js
@@ -22,7 +22,7 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     userId:{
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false,
     }
   });
