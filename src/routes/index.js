83d587817172218fb4a60403250654db76132606
unknown <guillermooscarn@gmail.com>
2023-04-11 22:42:18 -0300

    agregue createPayment con cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const {Router}=require('express');
 const router=Router();
 const vacantsRouter=require('./vacantsRoutes');
 const usersRouter = require('./usersRoutes');
+const premiumRouter=require('./premiumRoutes')
 
 router.use('/vacant', vacantsRouter);
 router.use('/user', usersRouter);
