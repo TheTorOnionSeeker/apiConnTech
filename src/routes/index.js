85e7b238e42964088cae6041d1b873b76c47bd4d
unknown <guillermooscarn@gmail.com>
2023-04-12 21:58:29 -0300

    agregue cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,12 @@ const vacantsRouter=require('./vacantsRoutes');
 const usersRouter = require('./usersRoutes');
 const premiumRouter=require('./premiumRoutes');
 const postulationRouter=require('./postulationRoutes');
+const notificationRouter=require('./notificationRoutes');
 
 router.use('/vacant', vacantsRouter);
 router.use('/user', usersRouter);
 router.use('/premium', premiumRouter);
 router.use('/postulation', postulationRouter);
-router.use('/notification')
+router.use('/notification', notificationRouter);
 
 module.exports = router;
