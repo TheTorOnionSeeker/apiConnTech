ff402721831c6e1040c30f2aa36cdc7e46f1811b
unknown <guillermooscarn@gmail.com>
2023-04-18 00:09:06 -0300

    agregue cambios

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(cors());
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://192.168.100.9:5173/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://192.168.100.9:5173/','http://localhost:5173/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
