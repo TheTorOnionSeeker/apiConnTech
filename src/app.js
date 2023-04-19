e58994491b413fe320999b3cf2af79a3a161a429
unknown <guillermooscarn@gmail.com>
2023-04-19 00:35:05 -0300

    agregue cambios

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(cors());
-server.use((req, res, next) => {
+/* server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
@@ -59,7 +59,7 @@ server.use((req, res, next) => {
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
-});
+}); */
 
 server.use("/", routes);
 
