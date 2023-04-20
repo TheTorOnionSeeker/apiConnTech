1a80fd86717317a6543bad359ec4dc948fba9015
Ema Schönborn <schonborn13@gmail.com>
2023-04-19 22:20:43 -0300

    m

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,7 @@
 const { User, Role, Education, Experience } = require("../db.js");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const tokenSecret = process.env.CRYPTED_TOKEN;
+const {ENCRYPTED_TOKEN} = process.env;
 
 const getUserById = async (req, res) => {
   const { id } = req.params;
@@ -58,7 +58,7 @@ const verifyUser = async (req, res) => {
   const tokenBytes = crypto.randomBytes(32);
   const tokenSecurity = tokenBytes.toString("hex");
   const hashedToken = crypto
-    .createHmac("sha256", tokenSecret)
+    .createHmac("sha256", ENCRYPTED_TOKEN)
     .update(tokenSecurity)
     .digest("hex");
 
@@ -82,9 +82,9 @@ const verifyUser = async (req, res) => {
     if (!user) throw new Error("User not found");
 
     //se envía token de autenticación creado, con expiración de 15 min, y datos de creación
-    // const tokenPayload = { userId: user.id, createdAt: Date.now() };
-    // const tokenOptions = { expiresIn: "1h" };
-    // const token = jwt.sign(tokenPayload, tokenOptions, hashedToken);
+    const tokenPayload = { userId: user.id, createdAt: Date.now() };
+    const tokenOptions = { expiresIn: "1h" };
+    const token = jwt.sign(tokenPayload, tokenOptions, hashedToken);
 
     res.status(201).json({ user: user, token: token, msg: "User found" });
   } catch (error) {
